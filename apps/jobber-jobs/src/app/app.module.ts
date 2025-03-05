43fd46f2853138bc38b020eae2d7aacda620a89b
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-05 20:56:09 +0530

    feat: add jobs resolver

--- apps/jobber-jobs/src/app/app.module.ts
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [
+    ConfigModule,
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
