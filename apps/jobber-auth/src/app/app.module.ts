5af919cb39654c5a593fc13e4860009ea5633dcb
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-05 14:06:08 +0530

    feat: setup graphql with nx lib [2-11]

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
