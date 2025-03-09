1cfa38f11ef94ba979e1ca1233090fde5cd875d4
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-09 23:16:42 +0530

    feat: configure grpc server [6-32]

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -4,10 +4,13 @@
  */
 
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
@@ -18,6 +21,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   const port = app.get(ConfigService).getOrThrow('PORT');
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
