c5410b4e09b2cfcf8938dff1c4cbfa8d3a7fbd2c
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-27 14:24:19 +0530

    feat: add message serialization and error handling [7-42]

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,6 +1,6 @@
 import { Producer } from 'pulsar-client';
 
-import { PulsarClient } from '@jobber/pulsar';
+import { PulsarClient, serialize } from '@jobber/pulsar';
 
 export abstract class AbstractJob<T> {
   private producer: Producer;
@@ -11,6 +11,6 @@ export abstract class AbstractJob<T> {
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
-    await this.producer.send({ data: Buffer.from(JSON.stringify(data)) });
+    await this.producer.send({ data: serialize(data) });
   }
 }
